8f593edc6fc6e0828f549caee40659de80e943fd
Dynas962 <33629719+Dynas962@users.noreply.github.com>
2018-05-05 12:07:27 -0400

    Agregando codigo

--- src/tumblr/Tumblr.java
+++ src/tumblr/Tumblr.java
@@ -1,12 +1,25 @@
 
 package tumblr;
 
+import com.tumblr.jumblr.JumblrClient;
+import com.tumblr.jumblr.types.User;
+
 
 public class Tumblr {
 
 
     public static void main(String[] args) {
-        System.out.println("Hola");
+        // Authenticate via OAuth
+        JumblrClient client = new JumblrClient(
+          "6CJI5nYV1FtCLgLaVrEJBv5ciZMNIlCrHof5KNe4wRPXn2TEYH",
+          "twEUONaIooBzP3w3FPkMTnHqWKw8AXTrHzPwI1HrqUyiQRM89u"
+        );
+        client.setToken(
+          "ZXjLGeY2jceU8X8r2WRSxdCdf73u6NONaqhbwdVlWyIeYMmZ3q",
+          "EBpBjNOfdjCVaKZTcSmfG7qds68zOonViDUxBMkycAsa7Gdgpe"
+        );
+
+        
     }
     
 }
